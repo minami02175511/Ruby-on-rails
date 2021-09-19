6ac9c16ed35d86e28157b030e17dc75c669adf20
EC2 Default User <ec2-user@ip-172-31-1-140.ec2.internal>
2021-09-19 01:29:13 +0000

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '4e824f63e7eba72b8c3eeb82e031886c0cd78df29a01d3ac7d6333792073ebea0bb0d1a31071b92ade3fdf6a554d9ff0947e813ca06e372d6d3f685781f6feac'
---
+++ db/migrate/20210912013926_addlmageld_tolists.rb
@@ -0,0 +1,4 @@
+class AddlmageldTolists < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
